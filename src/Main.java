7df9ae49a18924178a68c77e1dfaa183f52392a0
AndPrim <andreyminoss@gmail.com>
2023-10-22 20:38:42 +0500

    oneCommit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int cost = 1756;
+        int check = 20;
+        int bonus = cost/check;
+        System.out.println("Бонусов начислено: " + bonus);
+    }
+}
